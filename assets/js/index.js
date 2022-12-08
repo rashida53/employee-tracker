f2bb29b56ee95d7faf4c2207a0455d6a1592c77e
Rashida Kapadia <rashidamk21@gmail.com>
2022-12-07 22:53:00 -0600

    Add delete functions

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,7 +19,7 @@ const menu = {
     type: 'list',
     message: 'Please select from the following:',
     name: 'choice',
-    choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'View employees by manager', 'View employees by department', 'Update employee manager', 'View utilized budget of a department', 'Quit']
+    choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'View employees by manager', 'View employees by department', 'Update employee manager', 'View utilized budget of a department', 'Delete department', 'Delete role', 'Delete employee', 'Quit']
 };
 
 //Calls function based on option choosed from menu
@@ -27,10 +27,10 @@ function displayMenu() {
     inquirer.prompt(menu).then(response => {
         switch (response.choice) {
             case 'View all departments':
-                operate(viewDepartments);
+                viewDepartments();
                 break;
             case 'View all roles':
-                operate(viewRoles);
+                viewRoles();
                 break;
             case 'View all employees':
                 viewEmployees();
@@ -59,14 +59,19 @@ function displayMenu() {
             case 'View utilized budget of a department':
                 viewUtilizedBudget();
                 break;
+            case 'Delete department':
+                deleteDepartment();
+                break;
+            case 'Delete role':
+                deleteRole();
+                break;
+            case 'Delete employee':
+                deleteEmployee();
+                break;
         }
     })
 
 };
-
-function operate(operation) {
-    operation.apply();
-}
 
 function displayResults(results) {
     console.log('\n');
@@ -144,7 +149,7 @@ function addRole() {
     db.query(`SELECT id, name FROM department`, function (err, queryResults) {
         var roleQuestions = getRoleQuestions(queryResults);
         inquirer.prompt(roleQuestions).then(inquirerResponse => {
-            var departmentId = getDepartmentId(queryResults, inquirerResponse.choice);
+            var departmentId = extractId(queryResults, result => result.name === inquirerResponse.choice);
             let query = `INSERT INTO roles(title, salary, department_id) VALUES (?, ?, ?)`
             db.query(query, [inquirerResponse.title, inquirerResponse.salary, departmentId],
                 (err, results) => {
@@ -156,17 +161,13 @@ function addRole() {
     })
 }
 
-function getDepartmentId(queryResults, departmentName) {
-    return queryResults.filter(result => result.name === departmentName)[0].id;
-}
-
 function addEmployee() {
     db.query(`SELECT id, title FROM roles`, function (err, roleResults) {
         db.query(`SELECT id, CONCAT_WS(" ", first_name, last_name) AS manager_name FROM employees WHERE manager_id IS NULL`, function (err, managerResults) {
             var employeeQuestions = getEmployeeQuestions(roleResults, managerResults);
             inquirer.prompt(employeeQuestions).then(inquirerResponse => {
-                var roleId = getRoleId(roleResults, inquirerResponse.roleChoice);
-                var managerId = getManagerId(managerResults, inquirerResponse.managerChoice);
+                var roleId = extractId(roleResults, result => result.title === inquirerResponse.roleChoice);
+                var managerId = extractId(managerResults, result => result.manager_name === inquirerResponse.managerChoice);
                 let query = `INSERT INTO employees(first_name, last_name, manager_id, role_id) VALUES (?, ?, ?, ?)`
                 db.query(query, [inquirerResponse.firstName, inquirerResponse.lastName, managerId, roleId],
                     (err, rows) => {
@@ -213,40 +214,25 @@ function getEmployeeQuestions(roleResults, managerResults) {
     return employeeQuestions;
 };
 
-function getRoleId(roleResults, title) {
-    return roleResults.filter(result => result.title === title)[0].id;
-};
-
-function getManagerId(managerResults, manager_name) {
-    return managerResults.filter(result => result.manager_name === manager_name)[0].id;
-};
-
 function updateEmployeeRole() {
     db.query(`SELECT id, CONCAT_WS(" ", first_name, last_name) AS employee_name FROM employees`, function (err, employeeNameResults) {
         db.query(`SELECT id, title FROM roles`, function (err, updateRoleResults) {
             var updateEmployeeQuestions = getUpdateEmployeeQuestions(employeeNameResults, updateRoleResults);
             inquirer.prompt(updateEmployeeQuestions).then(inquirerResponse => {
-                var roleId = getUpdateRoleId(updateRoleResults, inquirerResponse.titleChoice);
-                var employeeId = getEmployeeId(employeeNameResults, inquirerResponse.nameChoice);
+                var roleId = extractId(updateRoleResults, result => result.title === inquirerResponse.titleChoice);
+                var employeeId = extractId(employeeNameResults, result => result.employee_name === inquirerResponse.nameChoice);
                 let query = `UPDATE employees SET role_id = ? WHERE id = ?`
                 db.query(query, [roleId, employeeId],
                     (err, rows) => {
                         if (err) throw err;
                         console.log(`Updated employee ${inquirerResponse.nameChoice} successfully in database`);
+                        displayMenu();
                     });
             });
         })
 
     })
 };
-
-function getUpdateRoleId(updateRoleResults, title) {
-    return updateRoleResults.filter(result => result.title === title)[0].id;
-};
-
-function getEmployeeId(employeeNameResults, fullName) {
-    return employeeNameResults.filter(result => result.employee_name === fullName)[0].id;
-}
 
 //Inquirer questions for update employee function
 function getUpdateEmployeeQuestions(employeeNameResults, updateRoleResults) {
@@ -274,9 +260,9 @@ function getUpdateEmployeeQuestions(employeeNameResults, updateRoleResults) {
 
 function viewEmployeesByManager() {
     db.query(`SELECT id, CONCAT_WS(" ", first_name, last_name) AS manager_name FROM employees WHERE manager_id IS NULL`, function (err, viewByManagerResults) {
-        var employeeByManagerQuestions = getEmployeeByManagerQuestions(viewByManagerResults);
+        var employeeByManagerQuestions = getInquirerQuestion('Select manager', viewByManagerResults, result => result.manager_name);
         inquirer.prompt(employeeByManagerQuestions).then(inquirerResponse => {
-            var managerId = getViewByManagerId(viewByManagerResults, inquirerResponse.managerChoice);
+            var managerId = extractId(viewByManagerResults, result => result.manager_name === inquirerResponse.choice);
             let query = `SELECT id, first_name, last_name FROM employees WHERE manager_id = ?`
             db.query(query, managerId,
                 (err, results) => displayResults(results));
@@ -284,56 +270,21 @@ function viewEmployeesByManager() {
     })
 };
 
-//Inquirer questions for view employee by manager function
-function getEmployeeByManagerQuestions(viewByManagerResults) {
-    var viewByManagerQuestions = [];
-    var managerNames = viewByManagerResults.map(result => result.manager_name);
-    var managerOptions = {
-        type: 'list',
-        message: 'Select your manager',
-        name: 'managerChoice',
-        choices: managerNames
-    }
-    viewByManagerQuestions.push(managerOptions);
-    return viewByManagerQuestions;
-};
-
-function getViewByManagerId(viewByManagerResults, managerName) {
-    return viewByManagerResults.filter(result => result.manager_name === managerName)[0].id;
-};
-
 function viewEmployeesByDept() {
     db.query(`SELECT id, name FROM department`, function (err, employeeByDeptResults) {
-        var employeeByDeptQuestions = getEmployeeByDeptQuestions(employeeByDeptResults);
+        var employeeByDeptQuestions = getInquirerQuestion('Select your department', employeeByDeptResults, result => result.name);
         inquirer.prompt(employeeByDeptQuestions).then(inquirerResponse => {
-            var deptName = getDeptName(employeeByDeptResults, inquirerResponse.deptChoice);
             let query = `SELECT employees.id, first_name, last_name, department.name AS department FROM employees 
                         JOIN roles
                         ON role_id = roles.id
                         JOIN department
                         ON department_id = department.id
                         WHERE department.name = ?`
-            db.query(query, deptName,
+            db.query(query, inquirerResponse.choice,
                 (err, results) => displayResults(results));
         })
     })
 };
-
-//Inquirer questions for view employee by department function
-function getEmployeeByDeptQuestions(employeeByDeptResults) {
-    var deptNames = employeeByDeptResults.map(result => result.name);
-    var employeeByDeptQuestions = {
-        type: 'list',
-        message: 'Select your manager',
-        name: 'deptChoice',
-        choices: deptNames
-    }
-    return employeeByDeptQuestions;
-};
-
-function getDeptName(employeeByDeptResults, deptName) {
-    return employeeByDeptResults.filter(result => result.name === deptName)[0].name;
-}
 
 function updateEmployeeManager() {
     db.query(`SELECT id, CONCAT_WS(" ", first_name, last_name) AS employee_name, manager_id FROM employees WHERE manager_id IS NOT NULL`, function (err, updateEmployeeManagerResults) {
@@ -341,13 +292,14 @@ function updateEmployeeManager() {
             db.query(`SELECT id, CONCAT_WS(" ", first_name, last_name) AS employee_name, manager_id FROM employees`, function (err, updateEmployeeManagerResultsThree) {
                 var updateEmployeeManagerQuestions = getUpdateEmployeeManagerQuestions(updateEmployeeManagerResults, updateEmployeeManagerResultsTwo);
                 inquirer.prompt(updateEmployeeManagerQuestions).then(inquirerResponse => {
-                    var managerId = getUpdateManagerId(updateEmployeeManagerResultsThree, inquirerResponse.managerChoice);
-                    var employeeId = getUpdateEmployeeId(updateEmployeeManagerResults, inquirerResponse.nameChoice);
+                    var managerId = extractId(updateEmployeeManagerResultsThree, result => result.employee_name === inquirerResponse.managerChoice);
+                    var employeeId = extractId(updateEmployeeManagerResults, result => result.employee_name === inquirerResponse.nameChoice);
                     let query = `UPDATE employees SET manager_id = ? WHERE id = ?;`
                     db.query(query, [managerId, employeeId],
                         (err, rows) => {
                             if (err) throw err;
                             console.log(`Updated employee manager for ${inquirerResponse.nameChoice} successfully in database`);
+                            displayMenu();
                         });
                 })
 
@@ -380,43 +332,84 @@ function getUpdateEmployeeManagerQuestions(updateEmployeeManagerResults, updateE
     return updateEmployeeManagerQuestions;
 };
 
-function getUpdateManagerId(updateEmployeeManagerResultsThree, manager_name) {
-    return updateEmployeeManagerResultsThree.filter(result => result.employee_name === manager_name)[0].id;
-};
-
-function getUpdateEmployeeId(updateEmployeeManagerResults, employee_name) {
-    return updateEmployeeManagerResults.filter(result => result.employee_name === employee_name)[0].id;
-};
 
 function viewUtilizedBudget() {
     db.query(`SELECT id, name FROM department`, function (err, utilizedBudgetResults) {
-        var selectDepartmentQuestions = getDepartmentNames(utilizedBudgetResults);
+        var selectDepartmentQuestions = getInquirerQuestion('Select department to view', utilizedBudgetResults, result => result.name);
         inquirer.prompt(selectDepartmentQuestions).then(inquirerResponse => {
-            var deptName = getDeptName(utilizedBudgetResults, inquirerResponse.deptChoice);
             let query = `SELECT department.name AS department, sum(salary) AS budget FROM employees 
-        JOIN roles
-        ON role_id = roles.id
-        JOIN department
-        ON department_id = department.id
-        WHERE department.name = ?`
-            db.query(query, deptName,
+                        JOIN roles
+                        ON role_id = roles.id
+                        JOIN department
+                        ON department_id = department.id
+                        WHERE department.name = ?`
+            db.query(query, inquirerResponse.choice,
                 (err, results) => displayResults(results));
         })
     })
 };
 
-function getDepartmentNames(utilizedBudgetResults) {
-    var deptNames = utilizedBudgetResults.map(result => result.name);
-    var selectDepartmentQuestion = {
-        type: 'list',
-        message: 'Select your department',
-        name: 'deptChoice',
-        choices: deptNames
-    }
-    return selectDepartmentQuestion;
+function deleteDepartment() {
+    db.query(`SELECT id, name FROM department`, function (err, deleteDepartmentResults) {
+        var deleteDepartmentQuestions = getInquirerQuestion('Select department to delete', deleteDepartmentResults, result => result.name);
+        inquirer.prompt(deleteDepartmentQuestions).then(inquirerResponse => {
+            let query = `DELETE FROM department where name = ?`
+            db.query(query, inquirerResponse.choice, (err, rows) => {
+                if (err)
+                    throw err;
+                console.log(`Deleted ${inquirerResponse.deptChoice} department successfully from database`);
+                displayMenu();
+            });
+        })
+    })
+};
+
+function deleteRole() {
+    db.query(`SELECT id, title FROM roles`, function (err, deleteRoleResults) {
+        var deleteRoleQuestions = getInquirerQuestion('Select role to delete', deleteRoleResults, result => result.title);
+        inquirer.prompt(deleteRoleQuestions).then(inquirerResponse => {
+            let query = `DELETE FROM roles where title = ?`
+            db.query(query, inquirerResponse.choice,
+                (err, rows) => {
+                    if (err) throw err;
+                    console.log(`Deleted ${inquirerResponse.choice} successfully from database`);
+                    displayMenu();
+                });
+        })
+    })
 };
 
 
+function getInquirerQuestion(question, results, extractValueFunction) {
+    var choices = results.map(extractValueFunction);
+    var inquirerQuestion = {
+        type: 'list',
+        message: question,
+        name: 'choice',
+        choices: choices
+    }
+    return inquirerQuestion;
+};
+
+function deleteEmployee() {
+    db.query(`SELECT id, CONCAT_WS(" ", first_name, last_name) AS employee_name FROM employees`, function (err, deleteEmployeeResults) {
+        var deleteEmployeeQuestions = getInquirerQuestion('Select employee to delete', deleteEmployeeResults, result => result.employee_name);
+        inquirer.prompt(deleteEmployeeQuestions).then(inquirerResponse => {
+            var employeeId = extractId(deleteEmployeeResults, result => result.employee_name === inquirerResponse.choice)
+            let query = `DELETE FROM employees WHERE id = ?`
+            db.query(query, employeeId,
+                (err, rows) => {
+                    if (err) throw err;
+                    console.log(`Deleted employee ${inquirerResponse.choice} successfully from database`);
+                    displayMenu();
+                });
+        });
+    })
+};
+
+function extractId(results, filterFunction) {
+    return results.filter(filterFunction)[0].id;
+};
 
 
 displayMenu();
